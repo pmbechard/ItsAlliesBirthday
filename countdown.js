34bf577c5e13df876c8b224bbaba4b126d685ef9
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-27 13:35:56 +0800

    Auto-updates birthday year for proceeding birthdays' countdowns (untested)

--- countdown.js
+++ countdown.js
@@ -1,7 +1,9 @@
 /********************  Countdown Module  **********************/
 
+let yearOfNextBirthday = 2022;
+
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 2, 2022 00:00:00").getTime();
+var countDownDate = new Date(`Apr 2, ${yearOfNextBirthday} 00:00:00`).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,8 +25,12 @@ var x = setInterval(function() {
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
-  if (distance < 0) {
+  // console.log(Math.floor(distance / (1000 * 60 * 60 * 24)));
+  if (days === 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "IT'S YOUR BIRTHDAY!";
+  } else if (days < 0) {
+    yearOfNextBirthday += 1;
+    x();
   }
 }, 1000);
