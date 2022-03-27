f954c98f4b9395d7b9862bac6f239e5bd8982ef9
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-27 14:32:11 +0800

    Adds random image module and cleans up style - responsiveness needs work

--- imageChooser.js
+++ imageChooser.js
@@ -1,1 +1,5 @@
 /********************  Image Module  **********************/
+
+let pictureIndex = Math.ceil(Math.random() * 28);
+const image = document.querySelector("#dicky-pic");
+image.setAttribute("src", `images/${pictureIndex}.jpg`);
